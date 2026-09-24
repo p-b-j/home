8ffeb3b7fffb953fd6095b1b5997f03f3d66adbb
Porter Jones <porter@mechanical-orchard.com>
2026-09-24 08:26:53 -0700

    Bump nixpkgs to unstable

--- nix/home/darwin/default.nix
+++ nix/home/darwin/default.nix
@@ -15,7 +15,7 @@
       apps = pkgs.buildEnv {
         name = "home-manager-applications";
         paths = config.home.packages;
-        pathsToLink = "/Applications";
+        pathsToLink = [ "/Applications" ];
       };
     in
     lib.mkIf pkgs.stdenv.targetPlatform.isDarwin "${apps}/Applications";
